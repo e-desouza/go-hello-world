8e80f5f8ca5bca77d51b72f4995c4cc4d99863ef
Vic Cross <48090243+viccross@users.noreply.github.com>
2021-07-26 15:51:13 +1000

    I can haz multiarch?

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!!!!!!!!!!!!!!!!!!")
+	fmt.Fprintf(w, "Hello, World, from %s on %s!!!", runtime.GOOS, runtime.GOARCH)
 }
